4e0578211f9e4c462fdca4e1b49c5e37e6972666
feldblume5263 <feldblume5263@gmail.com>
2022-03-21 08:19:36 +0900

    update codes

---
+++ test/addressTest.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main(void)
+{
+	int a = 42;
+	int b = 21;
+
+	printf("a = %d, b = %d\n", a, b);
+	swap(&a, &b);
+	printf("a = %d, b = %d\n", a, b);
+}
